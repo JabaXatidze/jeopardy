56dbd3dbf9cb1ffe1e2649cda4f8a4784508d7b2
Misha1204 <mikheil.kvashali@gmail.com>
2021-03-25 15:55:10 +0400

    main.py

--- main.py
+++ main.py
@@ -6,19 +6,30 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 
-# class Category(db.Model):
-#   id = db.Column(db.Integer, primary_key=True)
-#   title = db.Column(db.String(100), nullable=False)
+class Package(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  editor = db.Column(db.String(30))
+  author = db.Column(db.String(30), db.ForeignKey("user.id"), nullable=False)
+  tournament = db.Column(db.String)
 
-# class Question(db.Model):
-#   id = db.Column(db.Integer, primary_key=True)
-#   question = db.Column(db.String, nullable=False)
-#   point = db.Column(db.Integer)
-#   title_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+class Category(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(100), nullable=False)
+  package_id = db.Column(db.Integer, db.ForeignKey("package.id"), nullable=False)
+  questions = db.Column("Question")
 
-# class User(db.Model):
-#   id = db.Column(db.Integer, primary_key=True)
-#   username = db.column(db.String(20))
+class Question(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  question = db.Column(db.String, nullable=False)
+  point = db.Column(db.Integer)
+  title_id = db.Column(db.Integer, db.ForeignKey("category.id"), nullable=False)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(100), unique=True, nullable=False)
+  password = db.Column(db.String(25), nullable=False)
+  image = db.Column(db.String(20), nullable=False, default='default.jpg')
   
 
 
@@ -44,9 +55,11 @@ def register():
     return redirect(url_for('home'))
   return render_template("register.html", form=form)
 
-@app.route("/login") 
+@app.route("/login", methods=['GET', 'POST']) 
 def login():
   form = LoginForm()
+  if form.validate_on_submit():
+    return redirect(url_for('home'))
   return render_template("login.html", form=form)
 
 if __name__ == "__main__":
